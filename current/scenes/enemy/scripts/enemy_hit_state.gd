f0c3e9e6eac29ba06d5df1e61ac47852b086351c
saltchicken <john.eicher89@gmail.com>
2024-05-27 14:09:21 -0700

    Fix origin point and add sword for attack_down ALPHA

---
+++ current/scenes/enemy/scripts/enemy_hit_state.gd
@@ -0,0 +1,21 @@
+extends State
+
+@onready var character_body = self.get_owner()
+@onready var animation_tree = $"../../AnimationTree"
+
+func Enter():
+	animation_tree.get("parameters/playback").start('hit')
+	animation_tree.set("parameters/hit/BlendSpace2D/blend_position", character_body.direction_to_player)
+	character_body.velocity = -character_body.direction_to_player * 10.0
+	
+func Exit():
+	pass
+	
+func Update(_delta:float):
+	#print('still hit')
+	# TODO: Implement taking damage
+	pass
+
+func _on_animation_tree_animation_finished(anim_name):
+	if anim_name == 'hit':
+		state_transition.emit(self, 'idle') # TODO: Should this revert to the previous state and not just idle
---
+++ current/scenes/enemy/scripts/enemy_main.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+class_name Enemy
+@onready var state_machine = $StateMachine
+@onready var animation_tree = $AnimationTree
+@onready var player = get_tree().get_first_node_in_group('Players')
+
+@export var idle_direction = Vector2(0.0, 1.0)
+@export var health: int
+		
+@onready var distance_to_player
+@onready var direction_to_player
+
+@onready var attack_reach = 15
+@onready var attack_damage = 45
+
+func _ready():
+	add_to_group('Enemies')
+
+func _physics_process(delta):
+	distance_to_player = self.global_position.distance_to(player.global_position)
+	direction_to_player = self.global_position.direction_to(player.global_position)
+	move_and_collide(self.velocity * delta)
+
+func _get_hit(damage):
+	if state_machine.current_state.name != 'hit' and state_machine.current_state.name != 'death':
+		health -= damage
+		if health <= 0:
+			health = 0
+			state_machine.current_state.state_transition.emit(state_machine.current_state, 'death')
+		else:
+			state_machine.current_state.state_transition.emit(state_machine.current_state, 'hit')
+			
+
