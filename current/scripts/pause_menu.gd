2e3c0ed6a29c3a1997377c93c71fa5e668b4f954
saltchicken <john.eicher89@gmail.com>
2024-05-27 17:43:25 -0700

    Fix pause menu

--- current/scripts/pause_menu.gd
+++ current/scripts/pause_menu.gd
@@ -1,13 +1,32 @@
 extends Control
 
-@onready var game = $"../../.."
+func _ready():
+	self.visible = false
 
+func resume():
+	self.visible = false
+	get_tree().paused = false
+	
+func pause():
+	self.visible = true
+	get_tree().paused = true
+
+func _process(_delta):
+	captureEsc()
+	
+func captureEsc():
+	var esc = Input.is_action_just_pressed("escape")
+	if esc and !get_tree().paused:
+		pause()
+	elif esc and get_tree().paused:
+		resume()
+		
 func _on_resume_pressed():
-	game.pauseMenu()
+	resume()
 
 func _on_restart_pressed():
-	game.pauseMenu()
-	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
+	resume()
+	get_tree().reload_current_scene()
 
 func _on_quit_pressed():
 	get_tree().quit()
