b82f3ae5f42efc3bbf77c81f2fcfc2023193fe7c
saltchicken <john.eicher89@gmail.com>
2024-05-28 21:30:58 -0700

    @export Item in a chest

--- current/scenes/game/game_main.gd
+++ current/scenes/game/game_main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 func _ready():
+	# TODO: Put this in a better place
 	# Fix for ugly cursors in inventory
 	var can_drop_cursor_img = load("res://assets/cursors/invisible_cursor.png")
 	var forbidden_cursor_img = load("res://assets/cursors/invisible_cursor.png")
--- current/scenes/objects/chest/scripts/open_state.gd
+++ current/scenes/objects/chest/scripts/open_state.gd
@@ -6,7 +6,10 @@ extends State
 func Enter():
 	print('open now')
 	animation_tree.get("parameters/playback").travel('open')
-	character_body.player.inventory._collect_item("res://scenes/inventory/item/bow.tres")
+	if character_body.item:
+		character_body.player.inventory._collect_item(character_body.item.resource_path)
+	else:
+		print('no item')
 	#animation_tree.set("parameters/open/BlendSpace2D/blend_position", Vector2(0.0, 1.0))
 	pass
 	
