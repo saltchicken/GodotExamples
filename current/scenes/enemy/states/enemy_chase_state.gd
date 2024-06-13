78c27d28931d8d027dcfc57efc96586b7b9b4a60
saltchicken <john.eicher89@gmail.com>
2024-06-12 21:08:03 -0700

    Add close range leap to add to collision attack

--- current/scenes/enemy/states/enemy_chase_state.gd
+++ current/scenes/enemy/states/enemy_chase_state.gd
@@ -3,6 +3,9 @@ extends State
 @onready var character_body = self.get_owner()
 @onready var animation_tree = $"../../AnimationTree"
 @onready var attack_hitbox = $"../../CollisionAttackArea"
+@onready var close_attack_hitbox = $"../../CloseAttackArea"
+@onready var player_exists: bool = false
+@onready var player_exists_duration: float = 0.0
 
 func Enter():
 	animation_tree.get("parameters/playback").travel('chase')
@@ -11,8 +14,10 @@ func Enter():
 func Exit():
 	character_body.idle_direction = character_body.direction_to_player
 	
-func Update(_delta:float):
+func Update(delta:float):
 	#handle_attack_hitbox_direction()
+	handle_close_attack_hitbox_direction()
+	check_close_attack_range(delta)
 	check_attack_range()
 	animation_tree.set("parameters/chase/BlendSpace2D/blend_position", character_body.direction_to_player)
 	if character_body.distance_to_player < character_body.stats.chase_distance:
@@ -23,6 +28,26 @@ func Update(_delta:float):
 		
 #func handle_attack_hitbox_direction():
 	#attack_hitbox.position = character_body.direction_to_player * character_body.stats.attack_reach
+	
+func handle_close_attack_hitbox_direction():
+	close_attack_hitbox.position = character_body.direction_to_player * character_body.stats.attack_reach
+	
+func check_close_attack_range(delta):
+	var bodies = close_attack_hitbox.get_overlapping_bodies()
+	player_exists = false
+	if bodies:
+		for obj in bodies:
+			if obj.get_script() == Player:
+				player_exists = true
+	if player_exists:
+		player_exists_duration += delta
+	else:
+		player_exists_duration = 0.0
+	if player_exists_duration > 1.0:
+		player_exists_duration = 0.0
+		state_transition.emit(self, 'close_attack')
+				
+				
 	
 func check_attack_range():
 	var bodies = attack_hitbox.get_overlapping_bodies()
---
+++ current/scenes/enemy/states/enemy_close_attack.gd
@@ -0,0 +1,73 @@
+extends State
+
+@onready var character_body = self.get_owner()
+@onready var animation_tree = $"../../AnimationTree"
+@onready var attack_hitbox = $"../../CollisionAttackArea"
+@onready var close_attack_hitbox = $"../../CloseAttackArea"
+
+@onready var close_attack_duration: float = 0.0
+@onready var close_attack_direction
+
+func Enter():
+	print('close attack')
+	animation_tree.get("parameters/playback").travel('chase') # TODO Make a state for close_attack
+	animation_tree.set("parameters/chase/BlendSpace2D/blend_position", character_body.direction_to_player)
+	close_attack_duration = 0.0
+	close_attack_direction = character_body.direction_to_player
+	print(close_attack_direction)
+	
+	#character_body.velocity.x = character_body.direction_to_player.x * 10000
+	#character_body.velocity.y = character_body.direction_to_player.y * 10000
+	
+func Exit():
+	print('leaving close attack')
+	character_body.idle_direction = character_body.direction_to_player
+	
+func Update(delta:float):
+	check_attack_range()
+	if close_attack_duration < 0.5:
+		close_attack_duration += delta
+		print('speed')
+		character_body.velocity.x = close_attack_direction.x * 150
+		character_body.velocity.y = close_attack_direction.y * 150
+		
+		# # This will add some tracking towards the player
+		#character_body.velocity.x = character_body.direction_to_player.x * 150
+		#character_body.velocity.y = character_body.direction_to_player.y * 150
+	else:
+		#state_transition.emit(self, 'idle')
+		character_body.velocity.x = move_toward(character_body.velocity.x, 0.0, 2.0)
+		character_body.velocity.y = move_toward(character_body.velocity.y, 0.0, 2.0)
+		if character_body.velocity.x == 0 and character_body.velocity.y == 0:
+			state_transition.emit(self, 'idle')
+	#print(character_body.velocity)
+	#handle_attack_hitbox_direction()
+	#handle_close_attack_hitbox_direction()
+	
+	animation_tree.set("parameters/chase/BlendSpace2D/blend_position", character_body.direction_to_player)
+	
+	
+	
+	
+	#if character_body.velocity <= Vector2(0.25, 0.25):
+		#print("why")
+		#state_transition.emit(self, 'idle')
+		
+#func handle_attack_hitbox_direction():
+	#attack_hitbox.position = character_body.direction_to_player * character_body.stats.attack_reach
+	
+func handle_close_attack_hitbox_direction():
+	close_attack_hitbox.position = character_body.direction_to_player * character_body.stats.attack_reach
+	
+				
+				
+	
+func check_attack_range():
+	var bodies = attack_hitbox.get_overlapping_bodies()
+	if bodies:
+		#var closest_body = bodies.front() # TODO: Only interested in closest object?
+		for obj in bodies:
+			#print(obj)
+			if obj.get_script() == Player:
+				if obj.state_machine.current_state.name != 'hit' and obj.state_machine.current_state.name != 'death':
+					state_transition.emit(self, 'collision_attack')
