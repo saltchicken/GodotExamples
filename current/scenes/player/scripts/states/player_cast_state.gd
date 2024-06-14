e1bee1f70c6153b07eb2a9070c306ba022014248
saltchicken <john.eicher89@gmail.com>
2024-06-14 12:31:24 -0700

    Fix spell cast location

--- current/scenes/player/scripts/states/player_cast_state.gd
+++ current/scenes/player/scripts/states/player_cast_state.gd
@@ -12,6 +12,7 @@ func Enter():
 	animation_tree.set("parameters/cast/BlendSpace2D/blend_position", character_body.direction)
 	#character_body.velocity = -character_body.direction_to_player * 10.0
 	character_body.velocity = Vector2(0.0, 0.0)
+	cast_spell()
 	
 	
 func Exit():
@@ -24,6 +25,7 @@ func cast_spell():
 	if spell_selection_menu.current_selected_spell != null:
 		var current_spell = spell_selection_menu.current_selected_spell.instantiate()
 		current_spell.position = character_body.position + character_body.direction * 20 # TODO: Add attribute for spell location in reference to character_body
+		current_spell.position.y -= 15
 		get_tree().current_scene.add_child(current_spell)
 	else:
 		print('No spell selected')
@@ -32,7 +34,6 @@ func cast_spell():
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if anim_name in ["cast_left", "cast_right", "cast_up", "cast_down"]:
-		cast_spell()
 		if character_body.movement:
 			if character_body.run:
 				state_transition.emit(self, 'run')
