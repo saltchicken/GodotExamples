92dd2895ebe0c964ad1fd47d739317e358546f7f
saltchicken <john.eicher89@gmail.com>
2024-06-17 21:14:30 -0700

    Fix player direction when changed during an attack

--- current/scenes/player/states/player_sword_attack_state.gd
+++ current/scenes/player/states/player_sword_attack_state.gd
@@ -17,6 +17,7 @@ func Exit():
 	use_area_collision.disabled = false
 	
 func Update(_delta:float):
+	character_body.get_direction()
 	#character_body.velocity.x = move_toward(character_body.velocity.x, 0, DECELERATION_SPEED)
 	#character_body.velocity.y = move_toward(character_body.velocity.y, 0, DECELERATION_SPEED)
 	character_body.velocity = Vector2(0.0, 0.0)
